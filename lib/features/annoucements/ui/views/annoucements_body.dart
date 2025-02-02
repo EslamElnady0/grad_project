ed9c676ae142c3fa2a63fd6f03209bd2eb16637b
EslamElnady0 <eslamelnady254@gmail.com>
2025-02-02 22:58:33 +0200

    modified margins

--- lib/features/annoucements/ui/views/annoucements_body.dart
+++ lib/features/annoucements/ui/views/annoucements_body.dart
@@ -35,6 +35,7 @@ class AnnoucementsBody extends StatelessWidget {
             padding: EdgeInsets.symmetric(horizontal: 16.w),
             child: CoursesAnnoucementFilter(),
           ),
+          vGap(12),
           Expanded(child: AnnoucementsListView()),
         ],
       ),
