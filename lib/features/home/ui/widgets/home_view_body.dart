f75220b1e91626eb0c508b6d468c55ce4d7f3099
EslamElnady0 <eslamelnady254@gmail.com>
2025-02-02 18:38:48 +0200

    padding to column

--- lib/features/home/ui/widgets/home_view_body.dart
+++ lib/features/home/ui/widgets/home_view_body.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Padding(
+      padding: EdgeInsets.only(top: 22.h, right: 16.w, left: 16.w),
+      child: Column(
+        children: [],
+      ),
+    );
   }
 }
