255fd5832188ee8b52b0228f26398482a9eb3157
ibrahimsiam25 <ebrahimfathy2822@gmail.com>
2025-02-07 09:17:37 +0200

    Refactor CustomTextFormField to use DecoratedInputBorder

--- lib/core/widgets/custom_text_form_field.dart
+++ lib/core/widgets/custom_text_form_field.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:grad_project/core/widgets/decorated_input_border.dart';
 
 import '../../generated/l10n.dart';
 import '../helpers/localizationa.dart';
@@ -33,43 +34,30 @@ class _CustomTextFormFieldState extends State<CustomTextFormField> {
   @override
   Widget build(BuildContext context) {
     bool isArabic = isArabicLocale(context);
-    
-    return Container(
-      decoration: BoxDecoration(
-        color: AppColors.white,
-        borderRadius: BorderRadius.circular(12),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.1), // Adjust shadow color
-            blurRadius: 10, // Softness of the shadow
-            spreadRadius: 1, // Size of the shadow
-            offset: Offset(0, 2), // Position of shadow
-          ),
-        ],
-      ),
-      child: TextFormField(
-        obscureText: _isObscured,
-        onSaved: widget.onSaved,
-        keyboardType: widget.textInputType,
-        textAlign: isArabic ? TextAlign.right : TextAlign.left,
-        style: AppTextStyles.font10grayRegular,
-        validator: (value) {
-          if (value == null || value.isEmpty) {
-            return S.of(context).field_is_required;
-          }
-          return null;
-        },
-        decoration: InputDecoration(
-          hintText: widget.hintText,
-          hintStyle: AppTextStyles.font10grayRegular,
-          filled: true,
-          fillColor: Colors.transparent, // Make fillColor transparent
-          border: _buildBorder(),
-          enabledBorder: _buildBorder(),
-          focusedBorder: _buildBorder(),
-          suffixIcon: _buildPrefixIcon(),
-          contentPadding: const EdgeInsets.symmetric(vertical: 16, horizontal: 15),
-        ),
+
+    return TextFormField(
+      obscureText: _isObscured,
+      onSaved: widget.onSaved,
+      keyboardType: widget.textInputType,
+      textAlign: isArabic ? TextAlign.right : TextAlign.left,
+      style: AppTextStyles.font10grayRegular,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return S.of(context).field_is_required;
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+        hintText: widget.hintText,
+        hintStyle: AppTextStyles.font10grayRegular,
+        filled: true,
+        fillColor: Colors.transparent, // Make fillColor transparent
+        border: _buildBorder(),
+        enabledBorder: _buildBorder(),
+        focusedBorder: _buildBorder(),
+        suffixIcon: _buildPrefixIcon(),
+        contentPadding:
+            const EdgeInsets.symmetric(vertical: 16, horizontal: 15),
       ),
     );
   }
@@ -93,10 +81,18 @@ class _CustomTextFormFieldState extends State<CustomTextFormField> {
     return null;
   }
 
-  OutlineInputBorder _buildBorder() {
-    return OutlineInputBorder(
-      borderRadius: BorderRadius.circular(12),
-      borderSide: BorderSide.none,
+  DecoratedInputBorder _buildBorder() {
+    return DecoratedInputBorder(
+      child: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(12),
+        borderSide: BorderSide.none,
+      ),
+      shadow: BoxShadow(
+        color: Colors.black.withOpacity(0.1), // Adjust shadow color
+        blurRadius: 10, // Softness of the shadow
+        spreadRadius: 1, // Size of the shadow
+        offset: Offset(0, 2), // Position of shadow
+      ),
     );
   }
 
