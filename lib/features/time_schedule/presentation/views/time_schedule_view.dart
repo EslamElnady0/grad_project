fb93c6801f4257981a76afe6f3f76cd1f45c2ee5
Osama Selim <osamaselim216@gmail.com>
2025-04-19 00:41:40 +0200

    Created time schedule view and body

---
+++ lib/features/time_schedule/presentation/views/time_schedule_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/widgets.dart';
+import 'package:grad_project/core/widgets/custom_scaffold.dart';
+import 'package:grad_project/features/time_schedule/presentation/views/widgets/time_schedule_view_body.dart';
+
+class TimeScheduleView extends StatelessWidget {
+  const TimeScheduleView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScaffold(
+      body: TimeScheduleViewBody(),
+    );
+  }
+}
---
+++ lib/features/time_schedule/presentation/views/widgets/time_schedule_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class TimeScheduleViewBody extends StatelessWidget {
+  const TimeScheduleViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
