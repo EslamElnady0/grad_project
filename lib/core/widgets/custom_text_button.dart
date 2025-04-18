73106078fcaefb825f678a6e1874e237f0e71684
EslamElnady0 <eslamelnady254@gmail.com>
2025-04-18 18:24:46 +0200

    ss

--- lib/core/widgets/custom_text_button.dart
+++ lib/core/widgets/custom_text_button.dart
@@ -1,37 +1,35 @@
 import 'package:grad_project/core/theme/app_text_styles.dart';
-import 'package:grad_project/core/widgets/inner_shadow.dart';
-
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
-
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import '../theme/app_colors.dart';
 
 class CustomTextButton extends StatelessWidget {
   final String text;
- 
+
   final double? width;
   final VoidCallback onTap;
   final Widget? child;
   final TextStyle? style;
   final double? fontSize;
-  const CustomTextButton(
-      {super.key,
-      required this.text,
-      required this.onTap,
-      this.style,
-      this.child,
-      this.width, this.fontSize, });
+  const CustomTextButton({
+    super.key,
+    required this.text,
+    required this.onTap,
+    this.style,
+    this.child,
+    this.width,
+    this.fontSize,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: width ?? double.infinity,
-          padding: EdgeInsets.symmetric(vertical: 12.h),
+      padding: EdgeInsets.symmetric(vertical: 12.h),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(16.r),
-        gradient:const LinearGradient(
+        gradient: const LinearGradient(
           colors: [AppColors.primaryColorlight, AppColors.primaryColordark],
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
@@ -46,9 +44,10 @@ class CustomTextButton extends StatelessWidget {
       child: GestureDetector(
         onTap: onTap,
         child: Center(
-          child: Text(text, style: AppTextStyles.font20WhiteSemiBold.copyWith(
-            fontSize: fontSize?.sp,
-          )),
+          child: Text(text,
+              style: AppTextStyles.font20WhiteSemiBold.copyWith(
+                fontSize: fontSize?.sp,
+              )),
         ),
       ),
     );
