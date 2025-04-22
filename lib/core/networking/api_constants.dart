0bed9cd0171f3fb5ef2b434b8bbc484803dcaf49
ebrahimfathy2822@gmail.com <Siam7967>
2025-04-23 00:49:26 +0200

    feat: integrate Dio for networking and error handling
    - Added Dio as a dependency for HTTP requests.
    - Created ApiService to define API endpoints.
    - Implemented ApiErrorHandler to manage API errors.
    - Introduced ApiErrorModel to structure error responses.
    - Added ApiConstants for centralized API URL management.
    - Implemented DioFactory for configuring Dio instance with headers and interceptors.
    - Updated dependency injection to include ApiService.
    - Added constants for token management.
    - Created ApiResult class to handle success and failure responses.
    - Added necessary error constants for better error handling.
    - Updated pubspec.yaml and pubspec.lock to include new dependencies.

---
+++ lib/core/networking/api_constants.dart
@@ -0,0 +1,15 @@
+class ApiConstants {
+  static const String apiBaseUrl = "https://chifmate-rg.azurewebsites.net/api/";
+  static const String login = "auth/login";
+  static const String register = "auth/register";
+  static const String otpVerification = "otp-verification";
+  static const String resendOtp = "resend-otp";
+  static const String updateUserProfile = "user/update";
+  static const String loginWithGoogle = "auth/google";
+  static const String workSpace = "workspaces";
+  
+  static const String warehouse = "warehouse";
+  static const String ingredient = "ingredient";
+  static const String equipment = "equipment";
+}
+
