34ed0326d21b993ef9f919ea6eb8a97b3bf47de3
EslamElnady0 <eslamelnady254@gmail.com>
2025-04-28 01:51:53 +0300

    updated error dialog

--- lib/core/widgets/show_error_dialog.dart
+++ lib/core/widgets/show_error_dialog.dart
@@ -4,12 +4,8 @@ import 'package:grad_project/core/theme/app_text_styles.dart';
 import 'package:grad_project/generated/l10n.dart';
 import 'package:translator/translator.dart';
 
-import '../networking/api_error_model.dart';
-
-Future<void> showErrorDialog(
-    BuildContext context, ApiErrorModel apiErrorModel) async {
+Future<void> showErrorDialog(BuildContext context, String message) async {
   final translator = GoogleTranslator();
-  String message = apiErrorModel.getAllMessages();
 
   if (isArabicLocale(context)) {
     final translation =
@@ -17,28 +13,30 @@ Future<void> showErrorDialog(
     message = translation.text;
   }
 
-  showDialog(
-    context: context,
-    builder: (context) => AlertDialog(
-      icon: const Icon(
-        Icons.error,
-        color: Colors.red,
-        size: 32,
-      ),
-      content: Text(
-        message,
-        style: AppTextStyles.font16BlackSemiBold,
-        textAlign: TextAlign.center,
-      ),
-      actions: [
-        TextButton(
-          onPressed: () => Navigator.of(context).pop(),
-          child: Text(
-            S.of(context).ok,
-            style: AppTextStyles.font16BlackSemiBold,
-          ),
+  if (context.mounted) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        icon: const Icon(
+          Icons.error,
+          color: Colors.red,
+          size: 32,
         ),
-      ],
-    ),
-  );
+        content: Text(
+          message,
+          style: AppTextStyles.font16BlackSemiBold,
+          textAlign: TextAlign.center,
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: Text(
+              S.of(context).ok,
+              style: AppTextStyles.font16BlackSemiBold,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
