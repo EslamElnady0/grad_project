c76eb8d6516519fc393d73fdcd7fe9c451778acf
EslamElnady0 <eslamelnady254@gmail.com>
2025-04-28 14:01:18 +0300

    fixed the bloc provider issue

--- lib/core/helpers/constants.dart
+++ lib/core/helpers/constants.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
-import 'package:grad_project/core/di/dependency_injection.dart';
 import 'package:grad_project/core/helpers/app_assets.dart';
 import 'package:grad_project/core/theme/app_colors.dart';
 import 'package:grad_project/features/academic_progress/presentation/views/academic_progress_view.dart';
-import 'package:grad_project/features/annoucements/logic/get_announcement_cubit/get_announcement_cubit.dart';
-import 'package:grad_project/features/annoucements/logic/get_teacher_cources_cubit/get_teacher_cources_cubit.dart';
-import 'package:grad_project/features/annoucements/ui/ui%20cubit/announcement_filter_cubit.dart';
 import 'package:grad_project/features/annoucements/ui/views/annoucements_body.dart';
 import 'package:grad_project/features/dashboard/ui/views/dashboard_body.dart';
 import 'package:grad_project/features/final_results/presentation/views/final_results_view.dart';
@@ -238,20 +233,7 @@ class Constants {
   ];
   static List<Widget> adminHomeBodies = [
     const DashboardBody(),
-    MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) => getIt<GetAnnouncementCubit>(),
-        ),
-        BlocProvider(
-          create: (context) => getIt<GetTeacherCourcesCubit>(),
-        ),
-        BlocProvider(
-          create: (context) => AnnouncementFilterCubit(),
-        ),
-      ],
-      child: const AnnoucementsBody(),
-    ),
+    const AnnoucementsBody(),
     const LectureManagerView(),
     const Column(),
     const Column()
