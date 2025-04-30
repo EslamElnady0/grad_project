6098daf7bb03684be459db5080dfe7310f14f11d
EslamElnady0 <eslamelnady254@gmail.com>
2025-04-30 08:26:00 +0300

    fetch dashboard semesters

--- lib/features/dashboard/ui/views/dashboard_body.dart
+++ lib/features/dashboard/ui/views/dashboard_body.dart
@@ -1,38 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:grad_project/core/helpers/spacing.dart';
+import 'package:grad_project/core/logic/all_courses_cubit/all_courses_cubit.dart';
+import 'package:grad_project/features/dashboard/ui/cubit/get_dashboard_courses_cubit.dart';
 import 'package:grad_project/features/dashboard/ui/widgets/doctor_subjects_filter_row.dart';
 import 'package:grad_project/features/home/ui/widgets/title_text_widget.dart';
 import 'package:grad_project/generated/l10n.dart';
 import '../../../home/ui/widgets/home_screens_header_row.dart';
 import '../widgets/courses_info_list_view.dart';
 
-class DashboardBody extends StatelessWidget {
+class DashboardBody extends StatefulWidget {
   const DashboardBody({super.key});
+
+  @override
+  State<DashboardBody> createState() => _DashboardBodyState();
+}
+
+class _DashboardBodyState extends State<DashboardBody> {
+  @override
+  void initState() {
+    context.read<AllCoursesCubit>().get();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 16.w),
-      child: Column(
-        children: [
-          vGap(22),
-          HomeScreensHeaderRow(
-            onMenuTap: () {
-              Scaffold.of(context).openDrawer();
-            },
-            onSearchTap: () {},
-          ),
-          vGap(14),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.w),
-            child: TitleTextWidget(text: S.of(context).materials_center),
-          ),
-          vGap(12),
-          const DoctorSubjectsFilterRow(),
-          vGap(12),
-          const Expanded(child: CoursesInfoListView())
-        ],
+      child: BlocProvider(
+        create: (context) => GetDashboardCoursesCubit(),
+        child: Column(
+          children: [
+            vGap(22),
+            HomeScreensHeaderRow(
+              onMenuTap: () {
+                Scaffold.of(context).openDrawer();
+              },
+              onSearchTap: () {},
+            ),
+            vGap(14),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.w),
+              child: TitleTextWidget(text: S.of(context).materials_center),
+            ),
+            vGap(12),
+            const DoctorSubjectsFilterRow(),
+            vGap(12),
+            const Expanded(child: CoursesInfoListView())
+          ],
+        ),
       ),
     );
   }
