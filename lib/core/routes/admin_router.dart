48e0249cea99242edb29edfadc52095483db43c4
ibrahimsiam25 <ebrahimfathy2822@gmail.com>
2025-05-02 05:54:49 +0300

    fix: Correct initial location logic in AdminRouter for better routing

--- lib/core/routes/admin_router.dart
+++ lib/core/routes/admin_router.dart
@@ -26,8 +26,8 @@ import '../../features/subjects/ui/views/materials_view.dart';
 abstract class AdminRouter {
   static GoRouter getRouter(bool isLogin) {
     return GoRouter(
-      initialLocation:WeeklyScheduleView.routeName,
-      // isLogin ? HomeView.routeName : AuthView.routeName,
+      initialLocation:
+       isLogin ? HomeView.routeName : AuthView.routeName,
       routes: [
         GoRoute(
           path: HomeView.routeName,
