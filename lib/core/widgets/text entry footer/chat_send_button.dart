f0b0bd668e288ba3219249c3a1e11142dcf8118b
EslamElnady0 <eslamelnady254@gmail.com>
2025-05-20 00:31:35 +0300

    ..

--- lib/core/widgets/text entry footer/chat_send_button.dart
+++ lib/core/widgets/text entry footer/chat_send_button.dart
@@ -9,17 +9,14 @@ class ChatSendButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {},
-      child: Container(
-        padding: EdgeInsets.all(10.r),
-        decoration: BoxDecoration(
-            color: AppColors.primaryColordark,
-            borderRadius: BorderRadius.circular(12.r)),
-        child: SvgPicture.asset(
-          Assets.imagesSvgsSendIcon,
-          height: 20.h,
-        ),
+    return Container(
+      padding: EdgeInsets.all(10.r),
+      decoration: BoxDecoration(
+          color: AppColors.primaryColordark,
+          borderRadius: BorderRadius.circular(12.r)),
+      child: SvgPicture.asset(
+        Assets.imagesSvgsSendIcon,
+        height: 20.h,
       ),
     );
   }
