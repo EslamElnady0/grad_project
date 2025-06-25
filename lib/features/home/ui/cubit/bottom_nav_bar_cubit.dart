3cb9de898529e1541c7289205828c0413dc03307
ibrahimsiam25 <ebrahimfathy2822@gmail.com>
2025-06-25 21:41:21 +0300

    fix: Reset default currentIndex in BottomNavBarCubit to 0

--- lib/features/home/ui/cubit/bottom_nav_bar_cubit.dart
+++ lib/features/home/ui/cubit/bottom_nav_bar_cubit.dart
@@ -5,7 +5,7 @@ part 'bottom_nav_bar_state.dart';
 
 class BottomNavBarCubit extends Cubit<BottomNavBarState> {
   BottomNavBarCubit() : super(BottomNavBarInitial());
-  int currentIndex = 2;
+  int currentIndex = 0;
 
   void changeIndex(int index) {
     currentIndex = index;
