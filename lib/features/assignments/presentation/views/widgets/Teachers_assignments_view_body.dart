fb45ed6f28f02d7cee4e0a1cb813c06c4117ec6e
Osama Selim <osamaselim216@gmail.com>
2025-07-03 01:23:29 +0300

    added search bar to teacher_assignment_view

--- lib/features/assignments/presentation/views/widgets/Teachers_assignments_view_body.dart
+++ lib/features/assignments/presentation/views/widgets/Teachers_assignments_view_body.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:grad_project/core/helpers/spacing.dart';
 import 'package:grad_project/core/widgets/custom_inner_screens_app_bar.dart';
+import 'package:grad_project/core/widgets/custom_search_text_field.dart';
 import 'package:grad_project/features/assignments/data/models/get_assignments_request_query_params_model.dart';
 import 'package:grad_project/features/assignments/data/models/get_assignments_response_model.dart';
 import 'package:grad_project/features/assignments/logic/cubits/get_assignments_cubit/get_assignments_cubit.dart';
@@ -13,11 +14,43 @@ import 'package:grad_project/features/home/ui/widgets/title_text_widget.dart';
 import 'package:grad_project/generated/l10n.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 
-class TeachersAssignmentsViewBody extends StatelessWidget {
+class TeachersAssignmentsViewBody extends StatefulWidget {
   const TeachersAssignmentsViewBody(
       {super.key, required this.queryParamsModel});
 
   final GetAssignmentsRequestQueryParamsModel queryParamsModel;
+
+  @override
+  State<TeachersAssignmentsViewBody> createState() =>
+      _TeachersAssignmentsViewBodyState();
+}
+
+class _TeachersAssignmentsViewBodyState
+    extends State<TeachersAssignmentsViewBody> {
+  final TextEditingController _searchController = TextEditingController();
+  List<AssignmentModel> _filteredAssignments = [];
+  List<AssignmentModel> _allAssignments = [];
+
+  void _filterAssignments(String query) {
+    if (query.isEmpty) {
+      setState(() {
+        _filteredAssignments = _allAssignments;
+      });
+    } else {
+      setState(() {
+        _filteredAssignments = _allAssignments.where((assignment) {
+          return assignment.title.toLowerCase().contains(query.toLowerCase());
+        }).toList();
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -25,69 +58,59 @@ class TeachersAssignmentsViewBody extends StatelessWidget {
         vGap(22),
         symmetricPaddingWidget(
             child: CustomInnerScreensAppBar(
-                title: queryParamsModel.assignmentStatus == "scheduled"
+                title: widget.queryParamsModel.assignmentStatus == "scheduled"
                     ? S.of(context).scheduled_assignments
                     : S.of(context).previous_assignments)),
         vGap(12),
         symmetricPaddingWidget(
             horizontalPadding: 36.w,
             child: TitleTextWidget(
-                text: queryParamsModel.assignmentStatus == "scheduled"
+                text: widget.queryParamsModel.assignmentStatus == "scheduled"
                     ? S.of(context).scheduled_assignments_description
                     : S.of(context).previous_assignments_description)),
         vGap(10),
-        // symmetricPaddingWidget(
-        //     child: Row(
-        //   children: [
-        //     Expanded(
-        //       child: CustomTextFormField(
-        //         fillColor: AppColors.veryLightGray,
-        //         prefixIcon: Padding(
-        //           padding: const EdgeInsets.all(10),
-        //           child: SvgPicture.asset(
-        //             Assets.imagesSvgsMagnifyingGlassIcon,
-        //             colorFilter:
-        //                 const ColorFilter.mode(AppColors.gray, BlendMode.srcIn),
-        //           ),
-        //         ),
-        //         hintText: S.of(context).theSearch,
-        //         textInputType: TextInputType.text,
-        //       ),
-        //     ),
-        //     hGap(8),
-        //     CustomDropDownButton(
-        //       value: S.of(context).next_seven_days,
-        //       values: [S.of(context).next_seven_days],
-        //       onChanged: (value) {},
-        //     ),
-        //   ],
-        // )),
+        symmetricPaddingWidget(
+          horizontalPadding: 16.w,
+          child: CustomSearchTextField(
+            controller: _searchController,
+            onChanged: _filterAssignments,
+            hintText: S.of(context).theSearch,
+          ),
+        ),
         vGap(12),
         Expanded(
           child: BlocBuilder<GetAssignmentsCubit, GetAssignmentsState>(
-              builder: (context, state) => state.maybeWhen(
-                    getAssignmentsSuccess: (data) => _buildSuccessState(data),
-                    orElse: () => _buildLoadingState(),
-                  )),
+              builder: (context, state) {
+            return state.maybeWhen(
+              getAssignmentsSuccess: (data) {
+                _allAssignments = data.data;
+                if (_searchController.text.isEmpty) {
+                  _filteredAssignments = _allAssignments;
+                }
+                return _buildSuccessState(_filteredAssignments);
+              },
+              orElse: () => _buildLoadingState(),
+            );
+          }),
         )
       ],
     );
   }
 }
 
-Widget _buildSuccessState(GetAssignmentsResponseModel data) {
+Widget _buildSuccessState(List<AssignmentModel> data) {
   return ListView.separated(
     padding: EdgeInsets.only(top: 10.h),
     itemBuilder: (context, index) {
       return Padding(
         padding: EdgeInsets.symmetric(horizontal: 16.w),
         child: CustomAssignmentWidget(
-          assignmentModel: data.data[index],
+          assignmentModel: data[index],
         ),
       );
     },
     separatorBuilder: (context, index) => vGap(12),
-    itemCount: data.data.length,
+    itemCount: data.length,
     physics: const BouncingScrollPhysics(),
   );
 }
