2ad8da46214d33327730083d31d70dd49d254718
ibrahimsiam25 <ebrahimfathy2822@gmail.com>
2025-07-10 03:21:24 +0300

    refactor: Simplify QuizLabelWidgetContent layout by restructuring widget hierarchy

--- lib/features/quizes/ui/widgets/quiz_label_widget_content.dart
+++ lib/features/quizes/ui/widgets/quiz_label_widget_content.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:grad_project/core/helpers/app_assets.dart';
 import 'package:grad_project/core/helpers/constants.dart';
-import 'package:grad_project/core/helpers/extensions.dart';
 import 'package:grad_project/core/helpers/spacing.dart';
 import 'package:grad_project/core/theme/app_text_styles.dart';
 
@@ -16,36 +15,36 @@ class QuizLabelWidgetContent extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Row(
-          children: [
-            Container(
-              width: 42,
-              height: 42,
-              decoration: BoxDecoration(
-                gradient: Constants.secondaryGrad,
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8),
-                child: SvgPicture.asset(
-                  Assets.imagesSvgsQuizIcon,
-                  width: 25,
-                  height: 25,
+        Expanded(
+          child: Row(
+            children: [
+              Container(
+                width: 42,
+                height: 42,
+                decoration: BoxDecoration(
+                  gradient: Constants.secondaryGrad,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: SvgPicture.asset(
+                    Assets.imagesSvgsQuizIcon,
+                    width: 25,
+                    height: 25,
+                  ),
                 ),
               ),
-            ),
-            hGap(10),
-            SizedBox(
-              width: context.width * 0.4,
-              child: Text(
-                title,
-                style: AppTextStyles.font16BlackSemiBold,
-                maxLines: 5,
-                overflow: TextOverflow.ellipsis,
+              hGap(10),
+              Expanded(
+                child: Text(
+                  title,
+                  style: AppTextStyles.font16BlackSemiBold,
+                  maxLines: 5,
+                  overflow: TextOverflow.ellipsis,
+                ),
               ),
-            ),
-            hGap(10),
-          ],
+            ],
+          ),
         ),
         trailing ?? const SizedBox.shrink()
       ],
